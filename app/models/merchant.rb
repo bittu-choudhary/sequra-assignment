c39c9648d7ef798928844178f4c1839181e03dfd
Bittu Choudhary <bittuchoudhary007@gmail.com>
2023-03-22 19:22:43 +0530

    add has_many association from merchant to orders

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,7 @@
 class Merchant < ApplicationRecord
   belongs_to :currency
   has_many :disbursements
+  has_many :orders
   
   enum :live_on_weekday, { monday: 0, tuesday: 1, wednesday: 2, thursday: 3, friday: 4, saturday: 5, sunday: 6 }, prefix: true
   enum :disbursement_frequency, { daily: 0, weekly: 1 }, suffix: true
