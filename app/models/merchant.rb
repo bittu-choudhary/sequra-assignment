6bb082d542ea9cdfdf9bc5ec29d6e05a8cdf5a0e
Bittu Choudhary <bittuchoudhary007@gmail.com>
2023-03-22 18:50:31 +0530

    Add live on weekday enum values

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,4 @@
 class Merchant < ApplicationRecord
   belongs_to :currency
+  enum :live_on_weekday, { monday: 0, tuesday: 1, wednesday: 2, thursday: 3, friday: 4, saturday: 5, sunday: 6 }, prefix: true
 end
