ab6a3ecc4dd9e1962749c6997dbdf4f73d18d075
Bittu Choudhary <bittuchoudhary007@gmail.com>
2023-03-22 21:14:21 +0530

    Add has many association from merchant and tier plan to merchant tier plan table

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,7 +2,8 @@ class Merchant < ApplicationRecord
   belongs_to :currency
   has_many :disbursements
   has_many :orders
-  
+  has_many :merchant_tier_plans
+
   enum :live_on_weekday, { monday: 0, tuesday: 1, wednesday: 2, thursday: 3, friday: 4, saturday: 5, sunday: 6 }, prefix: true
   enum :disbursement_frequency, { daily: 0, weekly: 1 }, suffix: true
 end
--- app/models/tier_plan.rb
+++ app/models/tier_plan.rb
@@ -1,2 +1,4 @@
 class TierPlan < ApplicationRecord
+    has_many :merchant_tier_plans
+
 end
