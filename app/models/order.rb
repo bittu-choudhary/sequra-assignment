48468c8f1e840166f4d1d88ee2efe2b20c02b1e5
Bittu Choudhary <bittuchoudhary007@gmail.com>
2023-03-23 04:04:48 +0530

    remove null validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :merchant
-  belongs_to :disbursement
+  belongs_to :disbursement, optional: true
 
   enum :status, { pending: 0, completed: 1 }, prefix: true
 end
--- db/migrate/20230322135027_create_orders.rb
+++ db/migrate/20230322135027_create_orders.rb
@@ -2,7 +2,7 @@ class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders, id: :uuid do |t|
       t.belongs_to :merchant, null: false, foreign_key: true, type: :uuid
-      t.belongs_to :disbursement, null: false, foreign_key: true, type: :uuid
+      t.uuid :disbursement_id
       t.integer :status, null: false, default: 0
       t.boolean :is_part_of_disbursement, null: false, default: false
       t.datetime :disbursed_on
