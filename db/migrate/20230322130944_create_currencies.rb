4cb491c1a129fde20968a76204145a1608e783e6
Bittu Choudhary <bittuchoudhary007@gmail.com>
2023-03-22 18:46:37 +0530

    Add database validations for not null and uniqueness

--- db/migrate/20230322130944_create_currencies.rb
+++ db/migrate/20230322130944_create_currencies.rb
@@ -1,8 +1,8 @@
 class CreateCurrencies < ActiveRecord::Migration[7.0]
   def change
     create_table :currencies, id: :uuid do |t|
-      t.string :name
-      t.string :code
+      t.string :name, null: false, unique: true
+      t.string :code, null: false, unique: true
 
       t.timestamps
     end
