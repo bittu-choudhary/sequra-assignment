85d8ba2575f5a78f197847873c34608efff8ec83
Bittu Choudhary <bittuchoudhary007@gmail.com>
2023-03-26 01:38:17 +0530

    add pending_disbursement_calculation to query orders that are not considered for disbursement calculation

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,6 +11,7 @@ class Order < ApplicationRecord
 
   scope :completed, -> { where(status: 1) }
   scope :pending, -> { where(status: 0) }
+  scope :pending_disbursement_calculation, -> { where(disbursement_id: nil) }
 
   after_create :calculate_commission
 
