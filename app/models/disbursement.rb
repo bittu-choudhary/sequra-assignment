84b659840d10d29672a4e9aea8eb5e624c949055
Bittu Choudhary <bittuchoudhary007@gmail.com>
2023-03-26 01:22:09 +0530

    add presence validations on disbursement model

--- app/models/disbursement.rb
+++ app/models/disbursement.rb
@@ -2,5 +2,13 @@ class Disbursement < ApplicationRecord
   belongs_to :merchant
   has_many :orders
 
+  validates :merchant, presence: true
+  validates :gross_order_value, presence: true
+  validates :commission_amount, presence: true
+  validates :monthly_fee_amount, presence: true
+  validates :total_amount, presence: true
+  validates :status, presence: true
+
   enum :status, { in_progress: 0, ready: 1, completed: 2 }, prefix: true
+  
 end
