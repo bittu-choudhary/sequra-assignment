c0938c874bb6b3e1d213c2ca112f62b5f1ca1b22
Bittu Choudhary <bittuchoudhary007@gmail.com>
2023-03-26 01:22:55 +0530

    Add presence validations on order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,11 @@ class Order < ApplicationRecord
   belongs_to :merchant
   belongs_to :disbursement, optional: true
 
+  validates :merchant, presence: true
+  validates :disbursement, presence: true
+  validates :status, presence: true
+  validates :amount, presence: true
+
   enum :status, { pending: 0, completed: 1 }, prefix: true
+
 end
