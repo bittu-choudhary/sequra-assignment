b9770937215ffd3d08ccde6f90e64fdcc681fee3
Bittu Choudhary <bittuchoudhary007@gmail.com>
2023-03-26 01:18:38 +0530

    add presence and uniqueness validations on merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,6 +4,15 @@ class Merchant < ApplicationRecord
   has_many :orders
   has_many :merchant_tier_plans
 
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :live_on, presence: true
+  validates :live_on_weekday, presence: true
+  validates :disbursement_frequency, presence: true
+  validates :minimum_monthly_fee, presence: true
+  validates :currency, presence: true
+
   enum :live_on_weekday, { sunday: 0, monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5, saturday: 6 }, prefix: true
   enum :disbursement_frequency, { daily: 0, weekly: 1 }, suffix: true
+
 end
