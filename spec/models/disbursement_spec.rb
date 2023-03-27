8c5b40f86e2e2eeb516eb8eb372452de30e8f689
Bittu Choudhary <bittuchoudhary007@gmail.com>
2023-03-28 01:40:28 +0530

    add association and validation tests for disbursement spec

--- spec/models/disbursement_spec.rb
+++ spec/models/disbursement_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Disbursement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:orders) }
+    it { should belong_to(:merchant) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:merchant) }
+    it { should validate_presence_of(:gross_order_value) }
+    it { should validate_presence_of(:commission_amount) }
+    it { should validate_presence_of(:monthly_fee_amount) }
+    it { should validate_presence_of(:total_amount) }
+    it { should validate_presence_of(:status) }
+  end
+
 end
