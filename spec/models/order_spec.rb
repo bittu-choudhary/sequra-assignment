cfce49fe600307233d0446735c90367bfa8d9e23
Bittu Choudhary <bittuchoudhary007@gmail.com>
2023-03-28 01:35:42 +0530

    Add tests for order model scopes

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -11,4 +11,35 @@ RSpec.describe Order, type: :model do
     it { should validate_presence_of(:status) }
     it { should validate_presence_of(:amount) }
   end
+
+  describe 'scopes' do
+
+    let!(:complete_order) { create(:order, :completed) }
+    let!(:pending_order) { create(:order) }
+    let!(:undisbursed_order) { create(:order) }
+    let!(:merchant) { create(:merchant) }
+    let!(:received_by) { create(:order, merchant: merchant) }
+    let!(:created_between) { create(:order, created_at: Date.today - 5.day) }
+
+    it 'should return completed orders' do
+      expect(Order.completed).to include(complete_order)
+    end
+
+    it 'should return pending orders' do
+      expect(Order.pending).to include(pending_order)
+    end
+
+    it 'should return undisbursed orders' do
+      expect(Order.pending_disbursement_calculation).to include(undisbursed_order)
+    end
+
+    it 'should return orders own by' do
+      expect(Order.received_by(merchant)).to include(received_by)
+    end
+
+    it 'should return created between specific dates' do
+      expect(Order.created_between(Date.today - 10.day, Date.today)).to include(created_between)
+    end
+
+  end
 end
