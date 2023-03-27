0d51a8c0454cb5fcac9fd0116eac643719fdc4c1
Bittu Choudhary <bittuchoudhary007@gmail.com>
2023-03-28 01:37:32 +0530

    add tests for merchant model scopes

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -19,4 +19,32 @@ RSpec.describe Merchant, type: :model do
     it { should validate_presence_of(:email) }
   end
 
+  describe 'scopes' do
+    let!(:daily_disb_merchants) { create(:merchant) }
+    let!(:weekly_disb_merchants) { create(:merchant, :weekly_disbursement_freq) }
+    let!(:go_live_on_monday_merchants) { create(:merchant, live_on_weekday: "monday") }
+    let!(:live_on_after_merchants) { create(:merchant, live_on: Date.today) }
+    let!(:live_on_or_before_merchants) { create(:merchant, live_on: Date.today - 10.day) }
+
+    it "should return merchant with daily disbursement freq" do
+      expect(Merchant.with_daily_disbursement).to include(daily_disb_merchants)
+    end
+
+    it "should return merchant with weekly disbursement freq" do
+      expect(Merchant.with_weekly_disbursement).to include(weekly_disb_merchants)
+    end
+    
+    it "should return merchant that got live on monday" do
+      expect(Merchant.got_live_on_weekday("monday")).to include(go_live_on_monday_merchants)
+    end
+
+    it "should return merchants that got live after specific date" do
+      expect(Merchant.live_on_after(Date.today - 10.day)).to include(live_on_after_merchants)
+    end
+
+    it "should return merchants that got live on or before specific date" do
+      expect(Merchant.live_on_or_before(Date.today)).to include(live_on_or_before_merchants)
+    end
+  end
+
 end
