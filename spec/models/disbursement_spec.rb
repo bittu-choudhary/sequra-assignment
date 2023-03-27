5d2607965a5b8684d72bb5ec9f41632145bf0600
Bittu Choudhary <bittuchoudhary007@gmail.com>
2023-03-28 01:41:00 +0530

    add tests for disbursement model scopes

--- spec/models/disbursement_spec.rb
+++ spec/models/disbursement_spec.rb
@@ -15,4 +15,12 @@ RSpec.describe Disbursement, type: :model do
     it { should validate_presence_of(:status) }
   end
 
+  describe "scopes" do
+    let!(:disb_calculated_for_between) { create(:disbursement, calculated_for: Date.today - 5.day) }
+
+    it 'should return disbursements calculated for between specific dates' do
+      expect(Disbursement.calculated_for_between(Date.today - 10.day, Date.today)).to include(disb_calculated_for_between)
+    end
+  end
+
 end
