3b35fac9c2659eb187907669370e22f2cea0b8dc
Bittu Choudhary <bittuchoudhary007@gmail.com>
2023-03-28 01:35:13 +0530

    add association and validation tests for order spec

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'associations' do
+    it { should belong_to(:merchant) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:merchant) }
+    it { should validate_presence_of(:status) }
+    it { should validate_presence_of(:amount) }
+  end
 end
