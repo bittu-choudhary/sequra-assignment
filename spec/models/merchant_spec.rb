c59bac173811d9c36a13cadd770e77c904169d71
Bittu Choudhary <bittuchoudhary007@gmail.com>
2023-03-28 01:36:57 +0530

    add association and validation tests for merchant spec

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'associations' do
+    it { should belong_to(:currency) }
+    it { should have_many(:disbursements) }
+    it { should have_many(:orders) }
+    it { should have_many(:merchant_tier_plans) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:currency) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:live_on) }
+    it { should validate_presence_of(:live_on_weekday) }
+    it { should validate_presence_of(:disbursement_frequency) }
+    it { should validate_presence_of(:minimum_monthly_fee) }
+    it { should validate_presence_of(:email) }
+  end
+
 end
